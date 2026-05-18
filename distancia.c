15fd6d72926555b479d3ebbddd449b480631fb95
pnmarreto-tech <pnmarreto@gmail.com>
2026-05-18 13:46:08 -0300

    Add files via upload

---
+++ distancia.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+    int main (){
+        
+        int distancia, tempo;
+        scanf("%i", &distancia);
+        tempo = distancia * 2;
+        printf("%.0i minutos\n", tempo);
+
+        return 0;
+    }

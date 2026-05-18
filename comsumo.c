32e93c4915f1c213686cff2ac45e52ec26113697
pnmarreto-tech <pnmarreto@gmail.com>
2026-05-18 10:21:59 -0300

    Add files via upload

---
+++ comsumo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+    int main (){
+        int x;
+        float y,w;
+        scanf("%i", &x);
+        scanf("%f", &y);
+        w = (x/y);
+        printf("%.3f km/l\n", w);
+
+
+
+
+
+        return 0;
+    }

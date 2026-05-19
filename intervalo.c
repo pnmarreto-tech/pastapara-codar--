e461ee975b5235bc694fc9a436f8caade446430f
pnmarreto-tech <pnmarreto@gmail.com>
2026-05-19 14:18:41 -0300

    Add files via upload

---
+++ intervalo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+    int main(){
+        double numero;
+        scanf("%lf", &numero);
+        if(numero >=0.0 && numero <=25.0){
+            printf("Intervalo [0,25]\n");
+        }
+        else if(numero >25.0 && numero <=50.0){
+            printf("Intervalo  (25,50]\n");
+        }
+        else if(numero >50.0 && numero <=75.0){
+            printf("Intervalo (50,75]\n");
+        }
+        else if(numero >75.0 && numero <=100.0){
+            printf("Intervalo (75,100]\n");
+        }
+        else{
+            printf("Fora de intervalo\n");
+        }
+
+        return 0;
+    }
